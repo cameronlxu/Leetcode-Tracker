2561777ad2cdc0d23ddb9784c587589bc81a0750
Cameron Lau <cameron1998@gmail.com>
2023-01-06 00:44:54 -0800

    retrieve difficulty by link, update create/update user

--- AWS/lambdafunc.js
+++ AWS/lambdafunc.js
@@ -1,4 +1,5 @@
 const AWS = require("aws-sdk");
+var XMLHttpRequest = require('xhr2');
 AWS.config.update({ 
     region: "us-west-1"
 });
@@ -11,6 +12,15 @@ const RANKING_PATH = '/ranking';
 const DELETE_PATH = '/delete';
 const HEALTH_PATH = '/health';
 
+let problemObj = {
+    userId: '',
+    problem: {
+        link: '',
+        date: new Date().toLocaleDateString(),
+        difficulty: ''
+    }
+}
+
 exports.handler = async function(event) {
     console.log('Request event: ', event);
     let response;
@@ -19,7 +29,6 @@ exports.handler = async function(event) {
         case event.httpMethod === 'GET' && event.path === HEALTH_PATH:
             response = buildResponse(200);
             break;
-        // TODO: Complete these functions
         case event.httpMethod === 'POST' && event.path === COMPLETE_PATH:
             resposne = await createUser(JSON.parse(event.body));
             break;
@@ -41,9 +50,36 @@ exports.handler = async function(event) {
 }
 
 async function createUser(requestBody) {
+    const userId = requestBody['userId'];
+    const URL = requestBody['link'];
+
+    /**
+     * Setup the new user object with the completed problem inside the 'problems' array
+     */
+    // 1. Create the newUser object to add to DyanmoDB
+    let newUser = {
+        userId: userId,
+        problems: []
+    }
+
+    // 2. Create the problemObj to append to 'problems'
+    problemObj = {
+        link: URL,
+        date: new Date().toLocaleTimeString(),
+        difficulty: ''
+    }
+
+    getDifficulty(URL).then((difficulty) => {
+        problemObj.difficulty = difficulty;
+        newUser.problems.push(problemObj);
+    });
+
+    /**
+     * Add to DynamoDB
+     */
     const params = {
         TableName: TABLE_NAME,
-        Item: requestBody
+        Item: newUser
     }
     return await DYANMODB.put(params).promise().then(() => {
         const body = {
@@ -58,30 +94,33 @@ async function createUser(requestBody) {
 }
 
 async function updateUser(requestBody) {
-    /**
-     * "requestBody" Structure:
-     * 
-     * {
-     *      userId: ...,
-     *      problem: {
-     *          link: ...,
-     *          date: ...,
-     *          difficulty: ...
-     *      }
-     * }
-     */
+    const userId = requestBody['userId'];
+    const URL = requestBody['link'];
 
+    /**
+     * Setup problemObj with userId, URL, & difficulty
+     */
+    problemObj.userId = userId;
+    problemObj.problem.link = URL;
+
+    getDifficulty(URL).then((difficulty) => {
+        problemObj.problem.difficulty = difficulty;
+    });
+    
+    /**
+     * Prepare what to update in DynamoDB & send the update
+     */
     params = {
         TableName: TABLE_NAME,
         Key: {
-            'userId': requestBody['userId'],
+            'userId': userId,
         },
         UpdateExpression: "SET #problems = list_append(#problems, :val)",
         ExpressionAttributeNames: {
             "#problems": "problems"
         },
         ExpressionAttributeValues: {
-            ':val': requestBody['problem']
+            ':val': problemObj['problem']
         },
         ReturnValues: 'UPDATED_NEW'
     }
@@ -112,4 +151,38 @@ function buildResponse(statusCode, body) {
         },
         body: JSON.stringify(body)
     }
+}
+
+/**
+ * Functions to get the difficulty of a problem given the link
+ */
+
+// Source: https://stackoverflow.com/questions/247483/http-get-request-in-javascript 
+function httpGetAsync(URL) {
+  return new Promise((resolve, reject) => {
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            resolve(xmlHttp.responseText);
+        }
+    }
+    xmlHttp.open("GET", URL, true); // true for asynchronous 
+    xmlHttp.send(null); 
+  });
+}
+
+async function getDifficulty(URL) {
+    const text = await httpGetAsync(URL);
+
+    // Isolate the value for Difficulty: Easy/Medium/Hard
+    const findDifficulty = text.split('"difficulty"')[1];
+
+    // Remove the rest of the json data
+    const isolateDifficulty = findDifficulty.split(',')[0];
+    
+    // Remove the double quotations & semicolon with regex
+    const difficulty = isolateDifficulty.replace(/['":]+/g, '');
+
+    // Return the difficulty
+    return difficulty;
 }
